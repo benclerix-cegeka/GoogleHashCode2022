9ff38e8dafeb222d7c59f555597d503f2b66e5fa
Charlotte Schauvliege <charlotte.schauvliege@fluvius.be>
2022-02-24 19:51:10 +0100

    Added method HasEnoughSkills

--- GoogleHashCode2022/MoneyMachine/MoneyMachineCharlotte.cs
+++ GoogleHashCode2022/MoneyMachine/MoneyMachineCharlotte.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using GoogleHashCode2021;
+using GoogleHashCode2022.Models;
+using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace GoogleHashCode2022
 {
-    public class MoneyMachineCharlotte
+    public static class MoneyMachineCharlotte
     {
+        public static OutputData CalculateSolution(InputData input)
+        {
+            var output = new OutputData
+            {
+                Solutions = new List<Solution>()
+            };
+
+            Dictionary<string, Tuple<int, string>> skillOverview = new Dictionary<string, Tuple<int, string>>();
+            foreach (var contributor in input.Contributors)
+            {
+                foreach (var skill in contributor.Skills)
+                {
+                    skillOverview.Add(skill.Name, new Tuple<int, string>(skill.Level, contributor.Name));
+                }
+            }
+
+            foreach (var project in input.Projects)
+            {
+                foreach (var requiredSkill in project.RequiredSkills)
+                {
+                    input.Contributors.Find(c => c.Skills.Any(s => s.Name == requiredSkill.Name && s.Level >= requiredSkill.Level));
+                }
+
+                var contributors = input.Contributors;
+
+                var sln = new Solution
+                {
+                    ProjectName = project.Name,
+                };
+
+                output.Solutions.Add(sln);
+            }
+
+            return output;
+        }
+
     }
 }
