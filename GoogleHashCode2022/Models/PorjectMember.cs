d02c96fc01b3bfe1af489b68681fe68df5f254dd
Kenny Cegeka <kenny.broux@cegeka.com>
2022-02-24 20:27:12 +0100

    das zeker da

---
+++ GoogleHashCode2022/Models/PorjectMember.cs
@@ -0,0 +1,8 @@
+﻿namespace GoogleHashCode2022.Models
+{
+    public class ProjecMember
+    {
+        public string Name { get; set; }
+        public Skill Skill { get; set; }
+    }
+}
--- GoogleHashCode2022/Program.cs
+++ GoogleHashCode2022/Program.cs
@@ -15,7 +15,7 @@ namespace GoogleHashCode2021
             {
                 Console.WriteLine($"Calculating solution for file {file}");
                 var inputData = new InputData(file);
-                var outputData = MoneyMachineMaarten.CalculateSolution(inputData);
+                var outputData = MoneyMachineKenny.CalculateSolution(inputData);
                 outputData.WriteToOutputFile(Path.GetFileName(file));
             }
 
